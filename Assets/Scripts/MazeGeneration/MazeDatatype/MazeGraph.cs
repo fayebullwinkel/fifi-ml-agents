d3356fcb1cce94f49bf6312925f48de54ac4cfbe
Vivienne Drongowski <vividrongowski@gmail.com>
2023-09-16 18:58:10 +0200

    added logic for walls and neighbors to MazeManager + restructuring

--- Assets/Scripts/MazeGeneration/MazeDatatype/MazeGraph.cs
+++ Assets/Scripts/MazeGeneration/MazeDatatype/MazeGraph.cs
@@ -1,41 +1,40 @@
-﻿namespace MazeDatatype
+﻿using System.Collections.Generic;
+
+namespace MazeDatatype
 {
     public class MazeGraph
     {
-        private int _width;
-        private int _height;
-        
-        private MazeCell[,] _mazeCells;
-        
+        public int Width { get; }
+        public int Height { get; }
+
+        public MazeCell[,] Cells { get; }
+        public List<MazeWall> Walls { get; }
+
         public MazeGraph(int width, int height, bool withWalls = true)
         {
-            _width = width;
-            _height = height;
-            _mazeCells = new MazeCell[width, height];
+            Width = width;
+            Height = height;
+            Cells = new MazeCell[width, height];
+            Walls = new List<MazeWall>();
             
             // Initialize the maze cells
-            for (var x = 0; x < _width; x++)
+            for (var x = 0; x < Width; x++)
             {
-                for (var z = 0; z < _height; z++)
+                for (var z = 0; z < Height; z++)
                 {
-                    _mazeCells[x, z] = new MazeCell(x, z, withWalls);
+                    Cells[x, z] = new MazeCell(x, z, withWalls);
                 }
             }
         }
-        
-        public MazeCell[,] GetMazeCells()
-        {
-            return _mazeCells;
-        }
-        
+
         public MazeCell GetCell(int x, int z)
         {
-            return _mazeCells[x, z];
+            return Cells[x, z];
         }
-        
+
         public void SetCell(int x, int z, MazeCell cell)
         {
-            _mazeCells[x, z] = cell;
+            Cells[x, z] = cell;
         }
     }
 }
--- Assets/Scripts/MazeGeneration/MazeManager.cs
+++ Assets/Scripts/MazeGeneration/MazeManager.cs
@@ -1,14 +1,16 @@
-using System.Collections.Generic;
+using System;
 using MazeDatatype;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MazeManager : MonoBehaviour
 {
     [Header("Maze Settings")]
     [SerializeField]
-    private int x_Size;
+    private int xSize;
+
     [SerializeField]
-    private int z_Size;
+    private int zSize;
 
     [SerializeField]
     private float cellSize;
@@ -22,17 +24,16 @@ public class MazeManager : MonoBehaviour
 
     [SerializeField]
     private GameObject camera;
-    
+
     [SerializeField]
     private GameObject agentPrefab;
 
-    private MazeGraph _mazeGraph;
-    private List<GameObject> walls = new();
+    public MazeGraph mazeGraph { get; private set; }
 
     private void Start()
     {
         // Create a graph with walls in horizontal and vertical direction
-        _mazeGraph = new MazeGraph(x_Size, z_Size, withWalls: true);
+        mazeGraph = new MazeGraph(xSize, zSize, true);
         GenerateGrid();
         SetOuterWalls();
         SetCameraPosition();
@@ -41,89 +42,122 @@ public class MazeManager : MonoBehaviour
 
     private void GenerateGrid()
     {
+        var wallParent = CreateWallParent();
+
+        foreach (var cell in mazeGraph.Cells)
+        {
+            PlaceWall(cell, Wall.Top, wallParent, MazeWall.WallType.Horizontal);
+            PlaceWall(cell, Wall.Bottom, wallParent, MazeWall.WallType.Horizontal);
+            PlaceWall(cell, Wall.Left, wallParent, MazeWall.WallType.Vertical);
+            PlaceWall(cell, Wall.Right, wallParent, MazeWall.WallType.Vertical);
+        }
+    }
+
+    private GameObject CreateWallParent()
+    {
         var wallParent = new GameObject("wallParent");
         wallParent.transform.parent = transform;
         wallParent.transform.localPosition = new Vector3(0, 0.5f, 0);
+        return wallParent;
+    }
 
-        foreach (var cell in _mazeGraph.GetMazeCells())
+    private void PlaceWall(MazeCell cell, Wall wall, GameObject wallParent, MazeWall.WallType wallType)
+    {
+        if (!cell.GetWall(wall))
         {
-            if (cell.GetWall(Wall.Top))
-            {
-                var position = new Vector3(cell.GetX() * cellSize, 0, cell.GetZ() * cellSize + cellSize / 2);
-                if (!walls.Exists(x => x.transform.localPosition == position))
-                {
-                    var wall = Instantiate(wallPrefab, wallParent.transform);
-                    wall.transform.localPosition = position;
-                    wall.transform.localScale = new Vector3(cellSize, 1, 0.1f);
-                    walls.Add(wall);
-                }
-            }
-
-            // Place Bottom Wall if there is one
-            if (cell.GetWall(Wall.Bottom))
-            {
-                var position = new Vector3(cell.GetX() * cellSize, 0, cell.GetZ() * cellSize - cellSize / 2);
-                if (!walls.Exists(x => x.transform.localPosition == position))
-                {
-                    var wall = Instantiate(wallPrefab, wallParent.transform);
-                    wall.transform.localPosition = position;
-                    wall.transform.localScale = new Vector3(cellSize, 1, 0.1f);
-                    walls.Add(wall);
-                }
-            }
-
-            // Place Left Wall if there is one
-            if (cell.GetWall(Wall.Left))
-            {
-                var position = new Vector3(cell.GetX() * cellSize - cellSize / 2, 0, cell.GetZ() * cellSize);
-                if (!walls.Exists(x => x.transform.localPosition == position))
-                {
-                    var wall = Instantiate(wallPrefab, wallParent.transform);
-                    wall.transform.localPosition = position;
-                    wall.transform.localScale = new Vector3(0.1f, 1, cellSize);
-                    walls.Add(wall);
-                }
-            }
-
-            // Place Right Wall if there is one
-            if (cell.GetWall(Wall.Right))
-            {
-                var position = new Vector3(cell.GetX() * cellSize + cellSize / 2, 0, cell.GetZ() * cellSize);
-                if (!walls.Exists(x => x.transform.localPosition == position))
-                {
-                    var wall = Instantiate(wallPrefab, wallParent.transform);
-                    wall.transform.localPosition = position;
-                    wall.transform.localScale = new Vector3(0.1f, 1, cellSize);
-                    walls.Add(wall);
-                }
-            }
+            return;
         }
+        var position = GetWallPosition(cell, wall);
+        var existingWall = GetExistingWall(position);
+
+        if (existingWall == null)
+        {
+            CreateNewWall(cell, position, wallParent, wallType);
+        }
+        else
+        {
+            SetSecondCell(existingWall, cell);
+        }
+    }
+
+    private Vector3 GetWallPosition(MazeCell cell, Wall wall)
+    {
+        float xOffset = 0;
+        float zOffset = 0;
+
+        switch (wall)
+        {
+            case Wall.Top:
+                zOffset = cellSize / 2;
+                break;
+            case Wall.Bottom:
+                zOffset = -cellSize / 2;
+                break;
+            case Wall.Left:
+                xOffset = -cellSize / 2;
+                break;
+            case Wall.Right:
+                xOffset = cellSize / 2;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(wall), wall, null);
+        }
+
+        return new Vector3(cell.GetX() * cellSize + xOffset, 0, cell.GetZ() * cellSize + zOffset);
+    }
+
+    private MazeWall GetExistingWall(Vector3 position)
+    {
+        return mazeGraph.Walls.Find(x => x.gameObject.transform.localPosition == position);
+    }
+
+    private void CreateNewWall(MazeCell cell, Vector3 position, GameObject wallParent, MazeWall.WallType wallType)
+    {
+        var wall = Instantiate(wallPrefab, wallParent.transform);
+        wall.transform.localPosition = position;
+        wall.transform.localScale = GetWallScale(wallType);
+
+        var mazeWall = wall.GetComponent<MazeWall>();
+        mazeWall.Type = wallType;
+        mazeWall.Cell1 = cell;
+        mazeGraph.Walls.Add(mazeWall);
+    }
+
+    private void SetSecondCell(MazeWall wall, MazeCell cell)
+    {
+        wall.Cell2 = cell;
+        // Add neighbour to both cells
+        var cell1 = mazeGraph.GetCell(cell.GetX(), cell.GetZ());
+        var cell2 = mazeGraph.GetCell(wall.Cell1.GetX(), wall.Cell1.GetZ());
+        cell1.AddNeighbour(cell2);
+        cell2.AddNeighbour(cell1);
+    }
+
+    private Vector3 GetWallScale(MazeWall.WallType wallType)
+    {
+        return wallType == MazeWall.WallType.Horizontal ? new Vector3(cellSize, 1, 0.1f) : new Vector3(0.1f, 1, cellSize);
     }
 
     private void SetOuterWalls()
     {
         var mazeBounds = Instantiate(mazeBoundsPrefab, transform);
         var size = mazeBounds.transform.localScale;
-        size = new Vector3(size.x * cellSize * x_Size, size.y, size.z * cellSize * z_Size);
+        size = new Vector3(size.x * cellSize * xSize, size.y, size.z * cellSize * zSize);
         mazeBounds.transform.localScale = size;
         var position = mazeBounds.transform.localPosition;
-        position = new Vector3(x_Size - cellSize / 2, position.y, z_Size - cellSize / 2);
+        position = new Vector3(xSize - cellSize / 2, position.y, zSize - cellSize / 2);
         mazeBounds.transform.localPosition = position;
     }
 
     private void SetCameraPosition()
     {
-        camera.transform.localPosition = new Vector3(x_Size - cellSize / 2, (x_Size - cellSize / 2) * 2, -cellSize / 2);
+        camera.transform.localPosition = new Vector3(xSize - cellSize / 2, (xSize - cellSize / 2) * 2, -cellSize / 2);
     }
-    
+
     private void PlaceAgent()
     {
         var agent = Instantiate(agentPrefab);
-        agent.transform.localPosition = new Vector3(Random.Range(0, x_Size) * cellSize, 0.6f, Random.Range(0, z_Size) * cellSize);
-    }
-
-    public MazeGraph GetMazeGraph()
-    {
-        return _mazeGraph;
+        agent.transform.localPosition =
+            new Vector3(Random.Range(0, xSize) * cellSize, 0.6f, Random.Range(0, zSize) * cellSize);
     }
 }
