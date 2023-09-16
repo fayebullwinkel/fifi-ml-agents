b60577385c8ea3cc28a8ba3d211b3873552db551
Vivienne Drongowski <vividrongowski@gmail.com>
2023-09-16 18:57:09 +0200

    implemented MazeWall data type

---
+++ Assets/Scripts/MazeGeneration/MazeDatatype/MazeWall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace MazeDatatype
+{
+    public class MazeWall : MonoBehaviour
+    {
+        public enum WallType
+        {
+            Horizontal,
+            Vertical
+        }
+
+        public WallType Type { get; set; }
+        public MazeCell Cell1 { get; set; }
+        public MazeCell Cell2 { get; set; }
+
+        public MazeWall(WallType type, MazeCell cell1, MazeCell cell2)
+        {
+            Type = type;
+            Cell1 = cell1;
+            Cell2 = cell2;
+        }
+
+        public void DestroyWall()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
